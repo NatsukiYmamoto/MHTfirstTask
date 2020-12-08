26f13ba852178f4b1c6ed89795f93468ec53387a
Yanmo_swift <dntangfrvr@gmail.com>
2020-12-08 22:54:29 +0900

    赤と緑のオブジェクトが必要なわけではなく、【ビューの】ライフサイクル、ということで、画面遷移のことか、と気づいたがうまく画面が映らない。

--- firstHomework/AppDelegate.swift
+++ firstHomework/AppDelegate.swift
@@ -11,64 +11,26 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-//
-//
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        // Override point for customization after application launch.
-//        return true
-//    }
-//
-//    // MARK: UISceneSession Lifecycle
-//
-//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-//        // Called when a new scene session is being created.
-//        // Use this method to select a configuration to create the new scene with.
-//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-//    }
-//
-//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-//        // Called when the user discards a scene session.
-//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-//    }
-//
 
 
-    //ここから追記
-        var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
 
-        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // MARK: UISceneSession Lifecycle
 
-            print(NSStringFromClass(type(of:self)).components(separatedBy: ".")[1] + ":" + #function)
-            
-            window = UIWindow(frame: UIScreen.main.bounds)
-            window?.rootViewController = ViewController()
-            window?.makeKeyAndVisible()
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
 
-            return true
-        }
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
 
-        
-        func applicationWillResignActive(_ application: UIApplication) {
-            print(NSStringFromClass(type(of:self)).components(separatedBy: ".")[1] + ":" + #function)
-        }
-
-        func applicationDidEnterBackground(_ application: UIApplication) {
-            print(NSStringFromClass(type(of:self)).components(separatedBy: ".")[1] + ":" + #function)
-        }
-
-        func applicationWillEnterForeground(_ application: UIApplication) {
-            print(NSStringFromClass(type(of:self)).components(separatedBy: ".")[1] + ":" + #function)
-        }
-
-        func applicationDidBecomeActive(_ application: UIApplication) {
-            print(NSStringFromClass(type(of:self)).components(separatedBy: ".")[1] + ":" + #function)
-        }
-
-        func applicationWillTerminate(_ application: UIApplication) {
-            print(NSStringFromClass(type(of:self)).components(separatedBy: ".")[1] + ":" + #function)
-        }
-    //ここまで
-    
 }
 
